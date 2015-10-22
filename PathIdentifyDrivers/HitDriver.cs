0109920633a72e7e063d8d0f5abd602df2102091
Jimmy-WorkDesktop <hltzba@163.com>
2015-10-22 10:15:35 +0800

    初始化哈工大驱动调用文件

---
+++ PathIdentifyDrivers/HitDriver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DAL;
+using DataDict.Dicts;
+using DataDict.Models;
+
+namespace PathIdentifyDrivers
+{
+    /// <summary>
+    /// 哈工大抓拍设备驱动
+    /// </summary>
+    public class HitDriver:PathIdentifyDriverInterface
+    {
+
+    }
+}
